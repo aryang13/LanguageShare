4aa137552a427311564e82654c1145c48e0dea37
Allen Nguyen <anhn321@gmail.com>
2024-01-20 20:28:24 -0800

    Stylize navbar

--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -4,14 +4,50 @@ import { Outlet, NavLink } from 'react-router-dom';
 function NavBar() {
 	return (
 		<>
-			<div className='flex gap-4'>
-				<h1>Navbar</h1>
-				<NavLink to='/' className='btn'>
-					Home
-				</NavLink>
-				<NavLink to='/chats' className='btn'>
-					Chats
-				</NavLink>
+			<div className='navbar px-20 bg-base-100'>
+				<div className='flex-1'>
+					<NavLink to='/' className='btn btn-ghost text-2xl'>
+						LanguageLearner
+					</NavLink>
+				</div>
+				<div className='flex-none'>
+					<ul className='menu menu-horizontal px-1 '>
+						<li>
+							<NavLink to='/chats'>Chats</NavLink>
+						</li>
+					</ul>
+					<li className='dropdown dropdown-end'>
+						<div
+							tabIndex={0}
+							role='button'
+							className='btn btn-ghost btn-circle avatar'
+						>
+							<div className='w-10 rounded-full'>
+								<img
+									alt='Tailwind CSS Navbar component'
+									src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+								/>
+							</div>
+						</div>
+						<ul
+							tabIndex={0}
+							className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+						>
+							<li>
+								<a className='justify-between'>
+									Profile
+									<span className='badge'>New</span>
+								</a>
+							</li>
+							<li>
+								<a>Settings</a>
+							</li>
+							<li>
+								<a>Logout</a>
+							</li>
+						</ul>
+					</li>
+				</div>
 			</div>
 			<Outlet />
 		</>
